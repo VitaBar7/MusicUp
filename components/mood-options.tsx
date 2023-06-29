664bf3c5ff1c328ae1a02f06d0117d6d3d8cf14a
MJ Barba <mjbarbamar@gmail.com>
2023-06-29 21:53:04 +0200

    mood options popup, some styling

--- components/mood-options.tsx
+++ components/mood-options.tsx
@@ -10,6 +10,7 @@ export default function MoodOptions() {
     const[moodList, setMoodList] = useState<Mood[]>([])
     const [playlist, setPlaylist] = useState<Playlist>()
     const { userAccessToken } = useContext(AuthContext)
+    const [showModal, setShowModal] = useState(false);
 
    useEffect(() => {
     console.log(playlist)
@@ -25,10 +26,8 @@ export default function MoodOptions() {
             })
             setMoodList(temporaryMoodList)    
     }) 
-
     }, [])
 
-  
     const handleClick = (id:string) => {
         //console.log(id)
         if(id) {
@@ -38,31 +37,53 @@ export default function MoodOptions() {
               console.log(response)
             }
             )
-        }
-        
+        }  
     }
+
+   /*  const handleMouseEvent = (e:MouseEvent) => {
+      e.preventDefault()
+      const stickyButton = document.querySelector<HTMLElement>("#sticky")
+          let x = e.pageX
+          let y = e.pageY
+          //@ts-ignore
+          stickyButton.style.top = y + "px"
+          //@ts-ignore
+          stickyButton.style.left = x + "px"
+    } */
+
 
   return (
     <>
-    <main className="flex min-h-screen bg-dirty-white flex-col items-center justify-between p-24">
-        <div className=" bg-dark-green rounded-md font-sans font-thin m-auto px-24 pb-16">
-            <h1 className="text-2xl italic font-light tracking-wide text-dirty-white m-10">Pick your mood!</h1>
-            <ul className="text-dirty-white">
+      {!showModal ? (<button id="sticky" className="z-999 fixed top-40 whitespace-pre-line bg-orange/80 text-white rounded-full h-28 w-28 p-4 self-end hover:cursor-pointer shadow hover:opacity-75 transition" style={{height: '200'}} onClick={() => setShowModal(true)}>
+        How is <br></br>your mood?
+      </button>): null}
+      <div className="flex md:flex-col justify-center items-center sticky top-40 sm:mt-2 xs:top-16 ">
+       {showModal ? (<div className="bg-dark-grey/80 z-12 mt-4 sm:mt-2 xs:mt-2 text-thin rounded-md font-sans m-auto ">
+          <h1 className="text-2xl tracking-wide italic font-thin text-white whitespace-pre-line m-10 mt-12 sm:text-xl xs:text-lg">Pick your mood to get the right playlist!</h1> 
+          <div className="flex flex-col space-between">
+            <ul className="text-dirty-white px-24 xs:px-10 pt-5 pb-6 hover:backdrop-blur-2xl">
                 {moodList?.map((mood) => {
                     return (
                         <>
-                        <div className="flex">
-                            
-                            <li className="self-start mb-3"><button className="border me-auto text-dirty-white bg-dark-green rounded-full mr-2 px-2 mb-3 hover:bg-dirty-white hover:text-dark-green hover:px-3 hover:" onClick= {() =>{handleClick(mood.spotify_id)}}>&gt;</button> {mood.mood}
+                        <div className="flex">   
+                            <li className="self-start mb-3 font-thin xs:text-md"><button className="border border-white me-auto text-dirty-white bg-dark-grey rounded-full mr-2 px-2 mb-3 hover:bg-dirty-white hover:text-dark-grey hover:font-semibold hover:px-3" onClick= {() =>{handleClick(mood.spotify_id)}}>&gt;</button> {mood.mood}
                             </li>
                         </div>
                         </>
-                )}
+                    )}
                 )}
             </ul>
+            <button className="bg-close-icon bg-white bg-center bg-invert text-white rounded-full w-10 h-10 bg-no-repeat m-auto my-2 hover:border hover:border-dark-grey hover:rotate-45" onClick={() => setShowModal(false)}></button>
+            <p className="text-xs mb-2 font-light italic text-center tracking-wide">no playlist for me today, thanks</p>
+          </div>
+           {/*  <button
+              className="my-5 w-auto m-auto px-8 h-10 font-normal text-sm italic text-grey rounded-xl shadow hover:shadow-lg hover:text-orange hover:flex-end"
+              onClick={() => setShowModal(false)}>
+              Thanks, but no playlist today
+            </button> */}
         </div>
-
-    </main>
+        ): null}
+    </div>
     </>
   );
  
--- components/track-info.tsx
+++ components/track-info.tsx
@@ -44,35 +44,39 @@ export const TrackInfo = () => {
 
     return (
         <>
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <div className="mt-6 mb-32 max-h-fit grid text-center lg:mb-0 lg:grid-cols-1 lg:text-left">
-                <div className="group rounded-md border-transparent mx-1 p-1 transition-colors hover:border-gray-300 hover:bg-dirty-white hover:text-dark-grey">
+       {/*  <main className="flex min-h-screen flex-col items-center justify-between p-20"> */}
+            <div className="mb-12 max-h-fit grid text-center lg:mb-0 lg:grid-cols-2 md:grid-cols-2 xs:gap-y-4 lg:text-left">
+                <div className="grid rounded-md border-transparent grid-cols-1 mx-1 p-1  transition-colors hover:border-gray-300 bg-dirty-white text-dark-grey">
                     <img
                         className="rounded-sm relative dark:drop-shadow-[0_0_0.3rem_#ffffff70]"
                         src={trackDetails?.album?.images[0].url}
                         alt="album image"
                         
                         />
-                    <h2 className="mb-1 text-2xl tracking-wider hover:text-dark-grey">
+                    <h2 className="mb-1 text-2xl md:text-xl sm:text-lg xs:text-md tracking-wider text-dark-grey">
                         {trackDetails?.name} 
                     </h2>
-                    <p className= "m-0 mb-2 max-w-5xl hover:text-dark-grey text-thin text-md opacity-70">
-                        album: {trackDetails?.album?.name} 
+                    <p className= "m-0  max-w-5xl text-dark-grey md:text-sm sm:text-xs xs:text-xs text-thin text-md opacity-70">
+                        album: {trackDetails?.album?.name}  
                     </p>
                     <a href={trackDetails?.artists[0]?.external_urls.spotify} target="_blank" rel="noopener noreferrer">
-                        <p className= "m-0 mb-2 max-w-5xl text-xl text-thin tracking-wide hover:text-dark-grey  opacity-80 hover:text-normal">
+                        <p className= "m-0 mb-2 max-w-5xl text-xl md:text-md sm:text-sm xs:text-xs text-thin tracking-wide text-dark-grey  opacity-80 hover:text-normal">
                            by {trackDetails?.artists[0]?.name} 
                         </p>
                     </a>
 
-                    <a href={trackDetails?.external_urls.spotify} className="inline-flex items-center m-auto px-3 py-2 text-sm font-light text-center text-white border-white rounded-lg bg-dark-grey hover:cursor-pointer hover: ring-1 focus:outline-none focus:ring-light-pink dark:focus:ring-light-pink" target="_blank" rel="noopener noreferrer">
+                    <a href={trackDetails?.external_urls.spotify} type="button" className="inline-flex m-auto px-3 py-2 text-sm font-light text-center text-white border-white rounded-lg bg-dark-grey hover:cursor-pointer hover: ring-1 focus:outline-none focus:ring-light-pink dark:focus:ring-light-pink" target="_blank" rel="noopener noreferrer">
                         play in spotify
                         <svg aria-hidden="true" className="w-4 h-4 ml-2 mr-1 inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                     </a>
                 </div>
+                <div className="grid rounded-md border-transparent grid-cols-1 mx-1 p-1 transition-colors hover:border-gray-300 bg-dirty-white text-dark-grey">
+                    <h3>Song's lyrics will be here...</h3>
+                    <p className="text-xs overflow-y-auto">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sint ullam excepturi earum consequuntur repellat neque veniam distinctio, reprehenderit iure incidunt quam doloribus eligendi laudantium quibusdam mollitia? Id ab labore maiores.</p>
+                </div>
             </div>
 
-        </main>
+        {/* </main> */}
         </>
     )
 }
