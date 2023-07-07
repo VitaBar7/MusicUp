8845a2d1f2aa76ac1cf1d73e88c63159f641c24e
MJ Barba <mjbarbamar@gmail.com>
2023-07-07 17:48:17 +0200

    update css

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,18 @@
-import Image from 'next/image'
-import Link from 'next/link'
+
 import Navbar from '../components/navbar'
 import SearchSong from '@component/components/search-bar'
 import  LastTracks from '@component/components/last-tracks'
 import  MoodOptions  from '@component/components/mood-options'
 import AboutThis from '../components/about-this'
+import Head from 'next/head'
 
 export default function Home() {
   
   return (
+    <>
+    <Head>
+        <title>Look up!</title>
+    </Head>
     <main className="flex min-h-screen flex-col items-center justify-between py-20 px-24 sm:px-8 xs:max-sm:px-4 xs:max-sm:w-full">
       <Navbar></Navbar>
       <div className = "bg-album-playing absolute z-0 top-0 bg-fixed bg-no-repeat bg-cover w-full" style={{height:'100vh'}}>
@@ -19,6 +23,7 @@ export default function Home() {
       <LastTracks/> 
       <AboutThis/>
     </main>
+    </>
   )
 }
 
