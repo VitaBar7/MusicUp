1337c583616fc5e43885d702c82f391d3f81d9c5
Antoine Bourgeois <31954476+Arastorn@users.noreply.github.com>
2023-07-10 11:03:03 +0200

    Add login and player (#1)
    * add login
    
    * Add player for all pages and play trak or playlist
    
    * switch back to maria clientId
    
    ---------
    
    Co-authored-by: Antoine Bourgeois <antoine.bourgeois-ext@groupeseloger.com>

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import { AuthProvider } from '@component/context'
 import '@component/styles/globals.css'
 import type { AppProps } from 'next/app'
 import SupabaseProvider from '@component/providers/SupabaseProvider'
+import {WebPlayBackProvider} from "@component/context/webPlayBackContext";
 
 
 
@@ -11,7 +12,9 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     
       <AuthProvider>
-        <Component {...pageProps} />
+          <WebPlayBackProvider>
+            <Component {...pageProps} />
+          </WebPlayBackProvider>
       </AuthProvider> 
     
 
