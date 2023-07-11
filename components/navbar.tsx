324b7414c01fb3d84cb87aa601deb7e008043386
MJ Barba <mjbarbamar@gmail.com>
2023-07-11 14:59:34 +0200

    web player & spotify login's style

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -22,13 +22,18 @@ export default function Navbar() {
             <Link href='/'>
                 <h1 className="flex ml-6 font-medium text-white text-5xl tracking-wider xs:max-sm:text-3xl xs:max-sm:leading-6 ">Look up! </h1>
             </Link>
-            <ul className="flex flex-row flex-end gap-6 text-white mt-10 mr-10 xs:max-sm:mr-4 xs:max-sm:gap-4 xs:max-sm:text-sm hover:text-dirty-white">
-                <Link href='/'><li className=" text-white hover:text-dirty-white">Home</li></Link>
-                <Link href='/about'><li>About</li></Link>
-                <Link href="/blog"><li>Blog</li></Link>
-                {isUserAuthenticated && <li>{userDisplayName}</li>}
-                <li><SpotifyLoginButton></SpotifyLoginButton></li>
-            </ul>  
+            <div className="flex flex-col">
+                <ul className="flex order-last flex-row flex-end gap-6 text-white mt-4 mr-10 xs:max-sm:mr-4 xs:max-sm:gap-4 xs:max-sm:text-sm hover:text-dirty-white">
+                    <Link href='/'><li className=" text-white hover:text-dirty-white">Home</li></Link>
+                    <Link href='/about'><li>About</li></Link>
+                    <Link href="/blog"><li>Blog</li></Link>
+                </ul> 
+                <ul className="flex flex-row justify-around xs:max-sm:mr-3"> 
+                    {isUserAuthenticated && <li className="font-thin">Hello <span className="font-light">{userDisplayName}</span></li>}
+                    <li className="flex-end"><SpotifyLoginButton></SpotifyLoginButton></li>
+                </ul>
+
+            </div>
       </div>
         </>
     )
