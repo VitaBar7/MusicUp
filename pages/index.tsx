30b3f61b56b1583659f038e77cb5f8ba7c1c6719
MJ Barba <mjbarbamar@gmail.com>
2023-07-11 18:37:53 +0200

    player position homepage

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,9 +6,11 @@ import  LastTracks from '@component/components/last-tracks'
 import  MoodOptions  from '@component/components/mood-options'
 import AboutThis from '../components/about-this'
 import WebPlayBackSpotify from "@component/components/web-play-back-spotify";
+import { useContext } from 'react'
+import { WebPlayBackContext } from '@component/context'
 
 export default function Home() {
-  
+  const { current_track, player} = useContext(WebPlayBackContext)
   return (
     <>
     <Head>
@@ -20,9 +22,9 @@ export default function Home() {
       </div> 
       <SearchSong />
       <MoodOptions/>
-      <AboutThis/>
+      {(current_track && player) ? <WebPlayBackSpotify></WebPlayBackSpotify> :<AboutThis/>}
       <LastTracks/> 
-      <WebPlayBackSpotify></WebPlayBackSpotify>
+      
       
     </main>
     </>
