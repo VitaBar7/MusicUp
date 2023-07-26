c492d1eb1330c611ea486410618cb205e583359f
MJ Barba <mjbarbamar@gmail.com>
2023-07-26 10:10:54 +0200

    navigation buttons, mood button state, popup wip

--- api/user-profile.ts
+++ api/user-profile.ts
@@ -1,8 +1,8 @@
 import { GetUserInfoResponse } from './types'
 import { request }  from './utils'
 
-export const getUserInfo = async (userAccessToken:string):Promise<GetUserInfoResponse> => {
+export const getUserInfo = async (userAccessToken:string):Promise<GetUserInfoResponse | null > => {
     return request('https://api.spotify.com/v1/me', {}, userAccessToken)
         .then(response => response)
-        .catch(_ => "")
+        .catch(_ => null)
 }
--- components/spotify-login-button.tsx
+++ components/spotify-login-button.tsx
@@ -1,4 +1,4 @@
-import React, {useContext, useEffect, useState} from "react";
+import React, {useContext} from "react";
 import { AuthContext } from "@component/context"
 
 export const SpotifyLoginButton:React.FC = () => {
@@ -10,11 +10,13 @@ export const SpotifyLoginButton:React.FC = () => {
     const RESPONSE_TYPE = "token"
     const scope = "streaming user-read-email user-read-private"
 
+    const login = `${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${scope}`
+
     return (
         <div className="App">
                 {!isUserAuthenticated ?
                     <a className="bg-charbon rounded-full py-2 px-3 text-xs tracking-wider"
-                        href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${scope}`}>
+                        href={login}>
                             Login with Spotify</a>
                     : <button className="bg-charbon rounded-full py-2 px-3 text-xs tracking-wider hover:shadow-lg" onClick={logout}>Logout</button>}
         </div>
