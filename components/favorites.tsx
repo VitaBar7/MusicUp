e3e31a106bfdb5829eaa00be10433c299ebce50d
MJ Barba <mjbarbamar@gmail.com>
2023-08-01 13:18:09 +0200

    favorites: type, response fixed, and displayed.

--- components/favorites.tsx
+++ components/favorites.tsx
@@ -2,36 +2,83 @@
 import { AuthContext } from '@component/context'
 import { supabase } from '@component/utils/supabaseClient';
 import { useContext, useState, useEffect } from 'react';
+import { Favorite } from "@component/api/types"
+import Link from 'next/link'
+import Navbar from './navbar';
 
+type Favorites  = Favorite[]
 
 export default function Favorites () {
     const { userId, userAccessToken, isUserAuthenticated} = useContext(AuthContext)
-    const [favorites, setFavorites] = useState(null)
+    const [favorites, setFavorites] = useState<Favorites|undefined>(undefined)
+    const [fetchError, setFetchError] = useState("")
+
+    useEffect(()=> {
+        console.log(favorites)
+    }, [favorites])
 
     useEffect(() => {
-        getUserFavorites(userId) 
-        .then(()=>setFavorites)
+        getUserFavorites() 
+        .then((response)=>setFavorites(response))
     }, [userId])
     
-    async function getUserFavorites(userId:string) {
+    async function getUserFavorites():Promise<Favorites | undefined> {
       const { data, error } = await supabase
-      .from('favorites')
-      .select()
-      .eq('user_id', userId)
-      .order('id', { ascending: false })
-      .limit(24)
-    
+        .from('favorites')
+        .select()
+        .eq('user_id', userId)
+        .order('id', { ascending: false })
+        .limit(24)
+
       if(error) {
-        console.log(error)
+        console.log('No tracks here yet')
       }
+      if(data !== null && data.length > 0){
+        
+        return (data) as Favorites
+       }
+
+       return undefined
+    
+      
+    }
+      
+    return (
+        <>
+          {fetchError && (<p>{fetchError}</p>)}
+          {(favorites!==null && isUserAuthenticated)? 
+          <>
+          <Navbar/>
+          <div className = "user-tracks grid relative rounded-md mt-10 text-center backdrop-blur-2xl xs:grid-cols-2 sm:grid-cols-3 sm:gap-6 md:grid-cols-4 md:gap-6  lg:grid-cols-5 lg:gap-6 lg:text-left xl:grid-cols-6 xl:gap-8 xs:max-sm:gap-6 xs:max-sm:p-4 xs:max-sm:-mt-20 xs:max-sm:mb-12">
+          <h2 className="z-10 relative text-xl mt-6 pl-2 lg:text-xl lg:mt- md:self-start md:mt-10 ml-2 md:ml-0  sm:text-2xl sm:self-center  xs:text-md xs:max-sm:mt-4 xs:max-sm:mb-24 xs:self-start xs:text-[1.3rem] tracking-wider text-white">Your favorites:</h2>
+            {favorites?.map((item) => {
+              return (
+                <>
+                  <div className="text-white rounded-md backdrop-blur-2xl p-1 pb-1 shadow hover:cursor:pointer hover:border hover:border-taupe hover:drop-shadow-xl" key={item.id}>
+                    <Link 
+                        href={`track-details?id=${item.spotify_id}`} onClick={() => {}} >
+                        <img
+                        className="rounded-md"
+                        src={item.image}
+                        alt="album image"
+                        />
+                      <p className="text-xs text-center font-light tracking-wide mt-2 sm:text-center" key={item.id}>{item.track_name}
+                      <span className="not-italic font-thin"> by {item.artist_name}</span></p>
+                    </Link>
+                  </div>
+                </>
+              )
+            })}
+
+          </div>
+          </>
+        :null}
+    
+        </>
+    )
     }
 
 
-    return (
-        <>
-        </>
-    )
 
 
 
-}
--- pages/favorites.tsx
+++ pages/favorites.tsx
@@ -1,9 +1,14 @@
 import Favorites from '@component/components/favorites'
+import Head from 'next/head'
 
 export default function Favs() {
 
     return (
         <>
+        <Head>
+        <title>SongSeeker - Favorites</title>
+        </Head>
+        
         <main className="flex min-h-screen md:px-24 lg:px-28 xl:px-32 2xl:px-36 py-4 flex-col items-center xs:px-2">
             <Favorites />
 
