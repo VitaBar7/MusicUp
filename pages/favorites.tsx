f3ac01a7db39ecdb8527f872cc82fbba75a36bf2
VitaBar7 <104008965+VitaBar7@users.noreply.github.com>
2023-08-08 12:54:03 +0200

    Revert "name changed and logo added to navbar"
    This reverts commit 4c14355fff0cb22a1515f9c1488ea93859726b63.

--- pages/favorites.tsx
+++ pages/favorites.tsx
@@ -1,5 +1,4 @@
 import Favorites from '@component/components/favorites'
-import Navbar from '@component/components/navbar'
 import Head from 'next/head'
 
 export default function Favs() {
@@ -11,11 +10,7 @@ export default function Favs() {
         </Head>
         
         <main className="flex min-h-screen md:px-24 lg:px-28 xl:px-32 2xl:px-36 py-4 flex-col items-center xs:px-2">
-            <Navbar/>
-            <div className="mt-20">
-                <Favorites/>
-
-            </div>
+            <Favorites />
 
         </main>
         </>
