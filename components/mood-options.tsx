1dca6b7a9eb17ca1857a57fac0c8a552b959b34f
VitaBar7 <104008965+VitaBar7@users.noreply.github.com>
2023-08-08 12:54:03 +0200

    Revert "css responsive"
    This reverts commit 2e8588e346453014948b97658a8a946624db4de3.

--- components/mood-options.tsx
+++ components/mood-options.tsx
@@ -13,7 +13,7 @@ export default function MoodOptions() {
     const [showModal, setShowModal] = useState(false);
 
    useEffect(() => {
-    console.log(playlist?.id)
+    console.log(playlist)
    }, [playlist])
 
     useEffect(() => {
@@ -79,9 +79,7 @@ export default function MoodOptions() {
         </div>
         ): null}
     </div>
-     { playlist ? (<div>
-      {/* <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/1nN1vyVAWq06yNbc5W9y3L?utm_source=generator&theme=0" width="100%" height=“352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> */}
-     </div>): null}
+     { playlist ? (<div></div>): null}
     </>
   );
  
